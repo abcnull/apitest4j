bb95cca6971bf52079de82d5c168ee6980b12dd0
abcnull <abcnull@qq.com>
2020-08-13 23:15:48 +0800

    Initial commit

---
+++ src/test/java/util/ForTest.java
@@ -0,0 +1,13 @@
+package util;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author abcnull@qq.com
+ * @version 1.0.0
+ * @date 2020/8/13 22:57
+ */
+public class ForTest {
+
+}
